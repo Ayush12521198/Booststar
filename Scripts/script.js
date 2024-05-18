b7f46cb33313d56c68929a625dc21fc3ec293ee2
Rahul Razz <kunal98358@gmail.com>
2024-05-18 22:11:16 +0530

    Created Nav UI and Like btn  logics

---
+++ Scripts/script.js
@@ -0,0 +1,18 @@
+console.log("Rahul");
+const likeIcons = document.querySelectorAll(".like");
+var likeNum = 0;
+
+function likeOrNot(event) {
+  const clickedItem = event.target;
+  if (likeNum == 0) {
+    clickedItem.style.color = "red";
+    likeNum = 1;
+  } else if (likeNum == 1) {
+    clickedItem.style.color = "white";
+    likeNum = 0;
+  }
+}
+
+likeIcons.forEach((likeicon) => {
+  likeicon.addEventListener("click", likeOrNot);
+});
