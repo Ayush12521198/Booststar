e4f8b7195f8fe5c79aebb3846a881fbc7ab05b7c
Rahul Razz <kunal98358@gmail.com>
2024-05-19 18:10:27 +0530

    Created right section including search logic

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,4 @@
-console.log("Rahul");
+//  Logic for like btn start
 const likeIcons = document.querySelectorAll(".like");
 var likeNum = 0;
 
@@ -16,3 +16,23 @@ function likeOrNot(event) {
 likeIcons.forEach((likeicon) => {
   likeicon.addEventListener("click", likeOrNot);
 });
+// Logic for like btn ends
+
+// Logic for search btn display and hidden start 
+const srchBtn = document.querySelector("#leftSrchBtn");
+const srchInp = document.querySelector(".search");
+
+var srchBtnNum = 0;
+
+function disSrch(){
+  if (srchBtnNum == 0){
+srchInp.style.visibility = "visible";
+srchBtnNum = 1;
+  }else if (srchBtnNum == 1){
+    srchInp.style.visibility = "hidden";
+    srchBtnNum = 0;
+  }
+}
+
+srchBtn.addEventListener("click", disSrch);
+// Logic for search brn display and hidden end
